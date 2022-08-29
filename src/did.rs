0df0b74a1715f4b8bb8f2bf963b81a3b282b9810
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2022-08-29 09:09:41 +0200

    chore(did-web-server): simplify DIDSegment implementation

--- src/did.rs
+++ src/did.rs
@@ -123,17 +123,12 @@ impl DIDWeb {
     }
 }
 
-// TODO: wie implementiere ich einen Custom String Typ, der besondere Anforderungen / Traits
-// implemeniert / erfuellt?  Eigentlich geht es um die Daten im String, die bestimmten Ansprchen
-// genugen mssen
 #[derive(Debug)]
-struct DIDSegment {
-    segment: String,
-}
+struct DIDSegment(String);
 
 impl fmt::Display for DIDSegment {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.segment)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -142,9 +137,7 @@ impl DIDSegment {
         // ensure that each segment conforms to the DID Syntax specification: https://w3c.github.io/did-core/#did-syntax
         let re = Regex::new(r"^([a-zA-Z._-]|%[A-F][A-F])+$").unwrap();
         if re.is_match(segment) {
-            Ok(DIDSegment {
-                segment: segment.to_owned(),
-            })
+            Ok(DIDSegment(segment.to_owned()))
         } else {
             Err(DIDError::IllegalCharacter(
                 "segment contains illegal character".to_string(),
